aaca72735ccd5c1e1db5081a71ba367c1636d407
Ricardo Velasco <talo242@gmail.com>
2021-12-17 14:21:50 +0100

    refactor: separate actions and workflows

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/talo/i);
   expect(linkElement).toBeInTheDocument();
 });
